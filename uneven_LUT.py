2d7e73a016192c320dc58930e12425501fdd2059
Beiqi Li <beiqi.li@utexas.edu>
2024-11-27 16:32:55 -0600

    added Newton Raphson code

--- uneven_LUT.py
+++ uneven_LUT.py
@@ -106,7 +106,121 @@ def uneven_lut_index(bi):
         return (5, (bi >> 4) & 0b111)
     if bi >> 8 == 1:
         return (6, (bi >> 5) & 0b111)
-    return 
+    return (6, 0b111)
+
+import math
+
+N = 10  # For our case. We can later check for higher N values
+NUMLEVELS = math.ceil(math.log2(N))
+
+# Objective: To compute numerator/D
+
+# First need to normalize D
+# 0.5<= normalized_D <1
+# Establish a LUT for the Left Shift/Scaling Bits (Comparator based) (ceil(log2(N)) number of comparator levels and Scaling LUT size)
+
+# D is from 1 to N
+# if D is 1, numerator is our answer directly
+# Else:
+# First right shift D by ceil(log2(N)) bits to form scaledDownD
+# Now look up the Scaling LUT to left shift scaledDownD to form normalized_D
+# Now apply Newton Raphson to find 1/normalized_D
+# Now use the same scaling factor for the numerator and then multiply this 1/normalized_D by normalized_numerator
+
+def searchScalingLUT(scaledDownD):
+    leftShiftBits = 0
+    if scaledDownD == 1:
+        return 0
+    """    while scaledDownD < 1:
+        scaledDownD = scaledDownD * 2.0
+        leftShiftBits = leftShiftBits + 1
+    return leftShiftBits
+    """
+
+    if scaledDownD <= (2**-3) and scaledDownD > (2**-4):
+        leftShiftBits = 3
+    if scaledDownD <= (2**-2) and scaledDownD > (2**-3):
+        leftShiftBits = 2
+    if scaledDownD < (2**-1) and scaledDownD > (2**-2):
+        leftShiftBits = 1
+    if scaledDownD <= (2**0) and scaledDownD >= (2**-1):
+        leftShiftBits = 0
+    return leftShiftBits
+    """
+    # Implement using For Loop (Comparator-based)
+    leftShiftBits = NUMLEVELS - 1
+    lowerLimit = 2 ** (-NUMLEVELS)
+    for i in range(NUMLEVELS):
+        if scaledDownD > lowerLimit and scaledDownD <= 2 * lowerLimit:
+            return leftShiftBits
+        else:
+            leftShiftBits = leftShiftBits - 1
+            lowerLimit = lowerLimit * 2
+    """
+
+
+# returns normalized D and the required effective right scaling bits
+def NormalizeD(D):
+    if D == 1:
+        return 1, 0
+    else:
+        rightShiftBits = (
+            NUMLEVELS  # Also = number of comparator levels and Scaling LUT size
+        )
+        # scaledDownD = D >> rightShiftBits  # fixed point right shift
+        scaledDownD = D / (2**rightShiftBits)
+        # print(scaledDownD)
+        leftShiftBits = searchScalingLUT(scaledDownD)
+        # print(leftShiftBits)
+        # normalized_D = scaledDownD << leftShiftBits
+        normalized_D = scaledDownD * (2**leftShiftBits)
+        effectiveRightScalingBits = rightShiftBits - leftShiftBits
+        return normalized_D, effectiveRightScalingBits
+
+
+def NewtonRaphson(numerator, D):
+    normalized_D, effectiveRightScalingBits = NormalizeD(D)
+    # print(normalized_D)
+    # print(effectiveRightScalingBits)
+    normalized_numerator = numerator / (2**effectiveRightScalingBits)
+    # numerator >> effectiveRightScalingBits
+    # So that same scaling for numerator and denominator
+    # Now compute 1/normalized_D and then multiply with normalized_numerator
+    if normalized_D == 1:
+        return normalized_numerator
+    iterations = 3
+    X0 = 2.914 - 2 * normalized_D
+    Xi = X0
+    while iterations:
+        Xi_plus_1 = Xi * (2 - normalized_D * Xi)
+        Xi = Xi_plus_1
+        iterations = iterations - 1
+    oneByNormD = Xi
+    finalResult = oneByNormD * normalized_numerator
+    return finalResult
+
+
+def convert_num_to_9f(bi):
+    '''
+    Converts a number into the 9-bit fixed point format described at the top.
+    For debugging purpose only
+    
+    :param value: bi in Python floating point format
+    :return: bi in 9-bit fixed point format
+    '''
+    int_part = int(bi)
+    frac_part = bi - int_part
+    return int(int_part << 6 + int(frac_part * 64))
+
+def convert_6f_to_num(e):
+    '''
+    Converts a 6-bit fixed point number (data stored in LUT) into a regular floating point number.
+    For debugging purpose only
+    
+    :param value: e in 6-bit fixed point number format decribed above
+    :return: e in Python floating point format
+    '''
+    return e / 64
 
 if __name__ == '__main__':
     '''
@@ -119,7 +233,26 @@ if __name__ == '__main__':
     Fraction bits are 100001
     Thus, 2^bi is 33/64 in fraction format
     '''
-    bi = 0b000111111
-    LUT_indices = uneven_lut_index(bi)
-    print(LUT_indices)
-    print(bin(LUT[LUT_indices[0]][LUT_indices[1]]))
+    # bi = 0b000111111
+    # LUT_indices = uneven_lut_index(bi)
+    # print(LUT_indices)
+    # print(bin(LUT[LUT_indices[0]][LUT_indices[1]]))
+
+    '''
+    An example: softmax([0,1,2,3,4,5,6,7,8])
+    '''
+    z = [0,1,2,3,4,5,6,7,8]
+    # preprocess data: bi = zi - max(zj)
+    max_z = max(z)
+    b = [convert_num_to_9f(abs(zi - max_z)) for zi in z]
+    LUT_indices = [uneven_lut_index(bi) for bi in b]
+    e = [convert_6f_to_num(LUT[ind[0]][ind[1]]) for ind in LUT_indices]
+    sum_e = sum(e)
+    softmax = [NewtonRaphson(ei, sum_e) for ei in e]
+
+    golden_e = [2 ** zi for zi in z]
+    sum_golden_e = sum(golden_e)
+    golden_softmax = [golden_ei / sum_golden_e for golden_ei in golden_e]
+
+    print('Our Softmax: ',softmax)
+    print('Ground Truth Softmax: ', golden_softmax)
